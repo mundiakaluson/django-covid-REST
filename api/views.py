a7ae2b34704f98d059bbf9770443bc12352723e9
mundiakaluson <mundiakaluson@gmail.com>
2021-05-23 14:27:42 +0300

    heroku deployment

--- api/views.py
+++ api/views.py
@@ -6,14 +6,16 @@ import requests
 global url
 
 class CovidAPI(generics.ListAPIView):
-    
+    #issue a get request using django rest framework
     def get(self, request, *args, **kwargs):
+        # using a url of data loaded with covid data
         url = "https://coronavirus-19-api.herokuapp.com/countries"
         data = requests.get(url)
-
+        #rendering as json and giving an error response
         try:
             data = data.json()
         except:
             return Response({'error': "Something's not correct"})
         data = {"success": data}
+        #render the request through django rest framwork for easy visualization,
         return Response(data)
--- covid_api/urls.py
+++ covid_api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from api import views
 
+#render to the 'api' view class
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', views.CovidAPI.as_view()),
